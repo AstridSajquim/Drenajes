113bec42b38e41795fca451c672e2262bdd4c719
DESKTOP-8SHP5L7\dell <cr9pato@gmail.com>
2017-10-17 19:47:01 -0600

    agregación url cuadrillas

--- EmaxCuadrillas/urls.py
+++ EmaxCuadrillas/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^casos/', include('casos.urls', namespace='casos'))
+    url(r'^cuadrillas/', include('cuadrillas.urls', namespace='cuadrillas'))
 ]
