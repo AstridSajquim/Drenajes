74b4112eac9e741a8537d3c49bb80e868a15d413
Astrid <astrid.virginia1395@gmail.com>
2017-10-18 11:48:33 -0600

    Revert "Feature/app casos"

--- cuadrillas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from .views import IntegranteCreateView, FamiliarCreateView, CuadrillaCreateView, CuadrillaListView,IntegranteListView,
-
-urlpatterns = [
-    url(r'^tarea', TareasCreateView.as_view(), name='tarea_nueva'),
-    url(r'^nuevo', CasoCreateView.as_view(), name='caso_nuevo'),
-    url(r'^', CasoListView.as_view(), name='caso_lista')
